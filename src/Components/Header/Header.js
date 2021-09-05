f1bf11d8a674c80f419446503438aa4e80d24471
mariamchowdhury.200@gmail.com <mariamchowdhury.200@gmail.com>
2021-09-05 17:35:58 +0600

    complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Header.css";
+import red from "../images/gradient_img (1).png";
+import blue from "../images/blue.png";
+import green from "../images/green.png";
+import purple from "../images/purple.png";
 
 const Header = () => {
   return (
@@ -20,46 +24,111 @@ const Header = () => {
           </button>
         </div>
         <div className="container my-3">
-          <div className="row text-center ">
+          <div className="row  ">
             <div className="col-md-3 margin-class">
-              <div class="card  my-5 gray-font">
+              <div class="card my-5 gray-font">
                 <div class="card-body ">
-                  <div class="card-title d-flex justify-content-around">
-                    <button className="btn round-design">
-                      <span class="orange-circle">
-                        <h5>Followers / Following of @any_user</h5>
-                      </span>{" "}
-                    </button>
+                  <div class="card-title d-flex justify-content-evenly">
+                    <div className="div-design">
+                      <img src={red} className="gradient-circle " alt="" />
+                    </div>
+                    <h5>Followers / Following of @any_user</h5>
                   </div>
-                  <p class="card-text">
-                    Lorem Ipsum something is there. Something more will come
-                    here. Oh yes it will come.
+                  <p class="card-text ">
+                    <small>
+                      {" "}
+                      Lorem Ipsum something is there. Something more will come
+                      here. Oh yes it will come.
+                    </small>
                   </p>
-                  <a
-                    href="#"
-                    class="btn rectangle-button dark-orange-font fw-bold"
-                  >
-                    SAMPLE DATA
-                  </a>
+                  <div className="text-center">
+                    <a
+                      href="#"
+                      class="btn rectangle-button dark-orange-font fw-bold"
+                    >
+                      SAMPLE DATA
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
             <div className="col-md-3 margin-class">
-              <div class="card  my-5 gray-font">
+              <div class="card my-5 gray-font">
                 <div class="card-body">
-                  <h5 class="card-title">
-                    Liker / Commenter of <br /> any post
-                  </h5>
-                  <p class="card-text">
-                    Lorem Ipsum something is there. Something more will come
-                    here. Oh yes it will come.
+                  <div class="card-title d-flex justify-content-evenly">
+                    <div className="div-design">
+                      <img src={blue} className="gradient-circle" alt="" />
+                    </div>
+                    <h5> Liker / Commenter of any post</h5>
+                  </div>
+                  <p class="card-text ">
+                    <small>
+                      {" "}
+                      Lorem Ipsum something is there. Something more will come
+                      here. Oh yes it will come.
+                    </small>
                   </p>
-                  <a
-                    href="#"
-                    class="btn rectangle-button sky-blue-font fw-bold"
-                  >
-                    SAMPLE DATA
-                  </a>
+                  <div className="text-center">
+                    <a
+                      href="#"
+                      class="btn rectangle-button sky-blue-font fw-bold"
+                    >
+                      SAMPLE DATA
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row ">
+            <div className="col-md-3 margin-class">
+              <div class="card  my-5 gray-font">
+                <div class="card-body ">
+                  <div class="card-title d-flex justify-content-evenly">
+                    <div className="div-design">
+                      <img src={green} className="gradient-circle" alt="" />
+                    </div>
+                    <h5>People Posting Specific #Hashtags</h5>
+                  </div>
+                  <p class="card-text ">
+                    <small>
+                      {" "}
+                      Lorem Ipsum something is there. Something more will come
+                      here. Oh yes it will come.
+                    </small>
+                  </p>
+                  <div className="text-center">
+                    <a href="#" class="btn rectangle-button green-font fw-bold">
+                      SAMPLE DATA
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-3 margin-class">
+              <div class="card my-5 gray-font">
+                <div class="card-body">
+                  <div class="card-title d-flex justify-content-evenly">
+                    <div className="div-design">
+                      <img src={purple} className="gradient-circle" alt="" />
+                    </div>
+                    <h5>People Posting at Specific Locations</h5>
+                  </div>
+                  <p class="card-text ">
+                    <small>
+                      {" "}
+                      Lorem Ipsum something is there. Something more will come
+                      here. Oh yes it will come.
+                    </small>
+                  </p>
+                  <div className="text-center">
+                    <a
+                      href="#"
+                      class="btn rectangle-button purple-font fw-bold"
+                    >
+                      SAMPLE DATA
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
